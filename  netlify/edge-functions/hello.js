0c89fea3b8b601dd809616bd1c818b1b3662b5cc
Joshua Klein <klein.joshua.a@gmail.com>
2024-08-01 22:38:31 -0400

    first pass

---
+++  netlify/edge-functions/hello.js
@@ -0,0 +1,48 @@
+// netlify/edge-functions/hello.js
+import {
+    SplitFactory,
+    PluggableStorage,
+    ErrorLogger
+  } from "@splitsoftware/splitio-browserjs";
+
+export default async (request) => {
+    // Example condition: check a query parameter
+    const factory = SplitFactory({
+    core: {
+      authorizationKey: process.env.SPLIT_API_KEY,
+      key
+    },
+    mode: "consumer_partial",
+    storage: PluggableStorage({
+      wrapper: SplitStorageWrapper(process.env.SPLIT_API_KEY)
+    }),
+    // Disable or keep only ERROR log level in production, to minimize performance impact
+    debug: ErrorLogger()
+  });
+  const client = factory.client();
+
+  // Await until the SDK is ready or timed out, for which treatment evaluations will be 'control'.
+  // Timed out should never happen if SplitStorage durable object binding is properly configured.
+  await new Promise(res => {
+    client.on(client.Event.SDK_READY, res);
+    client.on(client.Event.SDK_READY_TIMED_OUT, res);
+  });
+
+  // Async evaluation, because it access the rollout plan from the Split Storage
+  const treatment = await client.getTreatment('my_flag');
+    let page;
+    if (treatment === 'on') {
+      page = 'alternative.html';
+    } else {
+      page = 'index.html';
+    }
+  
+    // Fetch the selected HTML page
+    const response = await fetch(new URL(`/${page}`, url.origin));
+    const htmlContent = await response.text();
+  
+    return new Response(htmlContent, {
+      headers: { 'Content-Type': 'text/html' },
+    });
+  };
+  
---
+++ functions/scheduled-function.js
@@ -0,0 +1,29 @@
+//const { Synchronizer } = require('@splitsoftware/splitio-sync-tools');
+import { Synchronizer } from "@splitsoftware/splitio-sync-tools";
+//const {PluggableStorage, ErrorLogger} from "@splitsoftware/splitio-browserjs"
+//const { Wrapper } = require("./SplitWrapper")
+//import Wrapper from "./SplitWrapper"
+import SplitStorageWrapper from "./SplitWrapper";
+
+const synchronizer = new Synchronizer({
+  core: {
+    authorizationKey: process.env.SPLIT_API_KEY
+  },
+  storage: {
+    // Wrapper object must implement the interface used by the Synchronizer to read and write data into an storage
+    type: 'PLUGGABLE',
+    wrapper: SplitStorageWrapper(process.env.SPLIT_API_KEY)
+  }
+});
+
+
+try {
+    await new Promise((res, rej) => {
+      synchronizer.execute(error => (error ? rej(error) : res()));
+    });
+    return new Response("Synchronization finished");
+  } catch (error) {
+    return new Response(`Synchronization failed with error: ${error}`, {
+      status: 500
+    });
+  }
